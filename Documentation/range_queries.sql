48917a98c8a213dca994ea13215238683ceedd24
Na Lee <nklee4@asu.edu>
2023-02-06 16:56:20 -0500

    Change range availability selection to include the other session if AM or PM session is booked in a day

--- Documentation/range_queries.sql
+++ Documentation/range_queries.sql
@@ -63,30 +63,79 @@ JOIN uses ON range_location.range_id = uses.range_id;
 /*
 VIEW RANGE REPORT - AVAILABLE RANGES ON A GIVEN DAY:
 UX will present option to view a range's report of all ranges available on a given day.
-UX will ask for the date of course.
-We are only doing by day and not by session b/c you need to account for range cleanup so we do not want to 
-double book a range.
 We will display all available ranges for that day for the report with this query:
 
 Parameter 1: course_date:DATE
 
 Example with hardcoded values:
-SELECT range_location.range_id, range_location.range_type, uses.in_session
+SELECT range_location.range_id,
+CASE
+WHEN sub_query.session_count = 1 THEN
+(SELECT CASE in_session
+WHEN 'AM' THEN 'PM'
+WHEN 'PM' THEN 'AM'
+ELSE 'NONE'
+END
+FROM uses
+WHERE uses.range_id = range_location.range_id
+AND uses.course_id IN (
+SELECT course.course_id
+FROM course
+WHERE course.course_date = '2023-01-30'
+)
+LIMIT 1
+)
+WHEN sub_query.session_count = 2 THEN 'NONE'
+ELSE 'AM & PM'
+END as session_available
 FROM range_location
-  LEFT JOIN uses ON range_location.range_id = uses.range_id
-  LEFT JOIN course ON uses.course_id = course.course_id
-WHERE range_location.range_id NOT IN (
-  SELECT range_id FROM uses WHERE course_id IN (
-    SELECT course_id FROM course WHERE course_date = '2023-01-30'));
+LEFT JOIN (
+SELECT uses.range_id,
+COUNT(uses.in_session) as session_count
+FROM uses
+JOIN course
+ON uses.course_id = course.course_id
+AND course.course_date = '2023-01-30'
+GROUP BY uses.range_id
+) sub_query
+ON range_location.range_id = sub_query.range_id
+LIMIT 0, 500
+
 */
 
-SELECT range_location.range_id, range_location.range_type, uses.in_session
+SELECT range_location.range_id,
+CASE
+WHEN sub_query.session_count = 1 THEN
+(SELECT CASE in_session
+WHEN 'AM' THEN 'PM'
+WHEN 'PM' THEN 'AM'
+ELSE 'NONE'
+END
+FROM uses
+WHERE uses.range_id = range_location.range_id
+AND uses.course_id IN (
+SELECT course.course_id
+FROM course
+WHERE course.course_date = '????-??-??'
+)
+LIMIT 1
+)
+WHEN sub_query.session_count = 2 THEN 'NONE'
+ELSE 'AM & PM'
+END as session_available
 FROM range_location
-LEFT JOIN uses ON range_location.range_id = uses.range_id
-LEFT JOIN course ON uses.course_id = course.course_id
-WHERE range_location.range_id NOT IN (
-SELECT range_id FROM uses WHERE course_id IN (
-SELECT course_id FROM course WHERE course_date ='????-?-??'));
+LEFT JOIN (
+SELECT uses.range_id,
+COUNT(uses.in_session) as session_count
+FROM uses
+JOIN course
+ON uses.course_id = course.course_id
+AND course.course_date = '????-??-??'
+GROUP BY uses.range_id
+) sub_query
+ON range_location.range_id = sub_query.range_id
+LIMIT 0, 500
+
 
 /* =====================================================================*/
 
