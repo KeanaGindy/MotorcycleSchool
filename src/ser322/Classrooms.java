e76521458d4ba85d85de43ad1ae14476fc28faac
Batbold Altansukh <baltansun@asu.edu>
2023-02-15 12:18:13 -0400

    add create and delete classroom

--- src/ser322/Classrooms.java
+++ src/ser322/Classrooms.java
@@ -7,9 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
 public class Classrooms extends Option implements OptionProtocol {
-    
 
     @Override
     public void openMenu(Connection conn, Scanner scr) {
@@ -18,8 +16,8 @@ public class Classrooms extends Option implements OptionProtocol {
         do {
             displayClassroomMenuOptions();
             userOpt = scr.nextLine();
-            System.out.println("You selected option: " + userOpt);  
-            //validate user input
+            System.out.println("You selected option: " + userOpt);
+            // validate user input
             switch (userOpt) {
                 case "1":
                     create(conn, scr);
@@ -28,7 +26,7 @@ public class Classrooms extends Option implements OptionProtocol {
                     view(conn);
                     break;
                 case "3":
-                    //update(conn, scr);
+                    // update(conn, scr);
                     break;
                 case "4":
                     delete(conn, scr);
@@ -39,9 +37,9 @@ public class Classrooms extends Option implements OptionProtocol {
                 default:
                     invalidInput();
                     break;
-            } 
+            }
         } while (isDone == false);
-        
+
     }
 
     public void displayClassroomMenuOptions() {
@@ -58,7 +56,7 @@ public class Classrooms extends Option implements OptionProtocol {
     @Override
     public void displayMenuOptions() {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
@@ -66,7 +64,7 @@ public class Classrooms extends Option implements OptionProtocol {
         // TODO Auto-generated method stub
 
         String queryStmt = "SELECT * from classroom_location";
-        
+
         try {
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(queryStmt);
@@ -74,20 +72,94 @@ public class Classrooms extends Option implements OptionProtocol {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        
+
     }
 
     @Override
     public void create(Connection conn, Scanner scr) {
         // TODO Auto-generated method stub
 
+        // Input Store Variables
+        String _classroom_id = null;
+        Integer _maxCapacity = null;
 
-        
+        // Statement & Duplicate Check Variables
+        PreparedStatement ps = null;
+        PreparedStatement psdc = null;
+        Boolean duplicate = null;
+
+        // Prompt for Input
+        System.out.print("Enter classroom id: \n");
+        _classroom_id = scr.next();
+        System.out.print("Enter classroom max capacity: \n");
+        _maxCapacity = scr.nextInt();
+
+        // Check for Duplicate
+        String lookupStmt = "SELECT * FROM range_location WHERE range_id = ?";
+        try {
+            psdc = conn.prepareStatement(lookupStmt);
+            psdc.setString(1, _classroom_id);
+            duplicate = checkDuplicate(psdc);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Return if Duplicate Found
+        if (duplicate) {
+            System.out.println("\nDUPLICATE ENTRY:: Range with provided ID already exists. Returning to menu.\n");
+            return;
+        }
+
+        // Proceed with Insertion Flow
+        String insertStmt = "INSERT INTO classroom_location VALUES (?, ?);";
+        try {
+            ps = conn.prepareStatement(insertStmt);
+            ps.setString(1, _classroom_id);
+            ps.setInt(2, _maxCapacity);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (updateDB(ps, conn)) {
+                System.out.println("Successfully added range to DB");
+            } else {
+                System.out.println("Failed to add range to DB");
+            }
+            try {
+                // Ensure Statement is Closed
+                if (ps != null) {
+                    ps.close();
+                }
+            } catch (SQLException se2) {
+                se2.printStackTrace();
+                System.out.println("Not all DB resources freed!");
+            }
+        }
     }
 
     @Override
     public void delete(Connection conn, Scanner scr) {
-        // TODO Auto-generated method stub
-        
+        // Input Store Variables
+        String _pk = null;
+
+        PreparedStatement ps = null;
+
+        System.out.println("Enter classroom to delete: pk(classroom_id):");
+        _pk = scr.next();
+
+        String deleteStmt = "DELETE FROM classroom_location WHERE classroom_id = ?";
+        try {
+            ps = conn.prepareStatement(deleteStmt);
+            ps.setString(1, _pk);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (updateDB(ps, conn)) {
+            System.out.println("Record deleted successfully.");
+        } else {
+            System.out.println("No records found to delete.");
+        }
+
     }
 }
